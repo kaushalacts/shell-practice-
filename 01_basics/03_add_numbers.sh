f968301430d945630e958cbc4a53f20c894013a0
Kaushalacts <kaushalkishoremnt2000@gmail.com>
2025-06-19 19:34:16 +0530

    problem 3 done.

---
+++ 01_basics/03_add_numbers.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "==============================================="
+echo "*******ADD TWO NUMBERS**********************"
+
+echo "Enter the first number:" 
+read num1
+echo "Enter the second number:" 
+read num2 
+
+sum= $((num1 + num2))
+
+echo "Sum of the two numbers is:" $sum
+
+
+echo "Happy Learning ! "
