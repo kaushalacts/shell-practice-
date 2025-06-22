18c163c0ddb3cbfd2c8bccdfc62da314748ba0b5
KAUSHAL KISHORE <59508627+kaushalacts@users.noreply.github.com>
2025-06-23 03:36:51 +0530

    Update 01_fibonacci_series.sh

--- 01_basics/Day-2/01_fibonacci_series.sh
+++ 01_basics/Day-2/01_fibonacci_series.sh
@@ -1,1 +1,30 @@
 
+#!/bin/bash
+
+echo "====================================================="
+echo "| Welcome to Day 2 of Linux Shell Scripting !       |"
+echo "====================================================="
+
+read -p  " Enter the number of terms" n
+
+# First two terms
+a=0
+b=1
+
+
+echo "Fibonacci Series upto $n terms"
+
+for((i=0;i<n;i++))
+
+do 
+    echo -n " $a "
+    fun=$((a+b))
+    a=$b
+    b=$fun
+done 
+
+echo " "
+
+echo "====================================================="
+echo "|             Happy Learning !                       |"
+echo "====================================================="
